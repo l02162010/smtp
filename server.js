3aee18010d08c00f89ac7c1044b6a3beac2a4d4c
Austin <l02162010@gmail.com>
2020-07-04 11:32:35 +0800

    change app port

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const app = require('./app');
 const config = require('config');
 console.log(config.app.port);
-module.exports = app.listen(process.env.PORT || config.app.port, process.env.HOST || config.app.host);
+module.exports = app.listen(3001);
